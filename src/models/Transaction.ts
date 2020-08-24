550b9f2062ad77f36ac890ffa400dba9e98deb76
Jonatas Maximiano <maximianojonatas@gmail.com>
2020-08-24 13:23:21 -0300

    Add cors and eager load

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -24,8 +24,10 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne( () => Category)
-  @JoinColumn({name: 'category_id'})
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+  
   category: Category
 
   @Column()
